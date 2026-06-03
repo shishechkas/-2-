46f2d189f0fdecc3e0d5ce1a1f4a2349d9051fc8
shishechkas <122203971+shishechkas@users.noreply.github.com>
2026-06-03 23:25:32 +0300

    добавил класс ingridient

---
+++ ingredient.py
@@ -0,0 +1,28 @@
+class Ingredient:
+    
+    def __init__(self, name, quantity, unit):
+        self.name = name
+        self.unit = unit
+        self.quantity = quantity
+
+    @property
+    def quantity(self):
+        return self._quantity
+
+    @quantity.setter
+    def quantity(self, value):
+        if value <= 0:
+            raise ValueError('Количество должно быть положительным')
+        self._quantity = float(value)
+
+    def __str__(self):
+        return f'{self.name}: {self.quantity} {self.unit}'
+
+    def __repr__(self):
+        return f"Ingredient('{self.name}', {self.quantity}, '{self.unit}')"
+
+    def __eq__(self, other):
+        if isinstance(other, Ingredient):
+            return self.name == other.name and self.unit == other.unit
+        else:
+            return False
